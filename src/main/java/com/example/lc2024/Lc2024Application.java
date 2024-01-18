9de51a24af32ab754e051f3fe6123ce38e42846c
real-folkblues <marenicolor@gmail.com>
2024-01-18 12:02:25 -0500

    init springboot, gradle

---
+++ src/main/java/com/example/lc2024/Lc2024Application.java
@@ -0,0 +1,13 @@
+package com.example.lc2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lc2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lc2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lc2024/Lc2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lc2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lc2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
